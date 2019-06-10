14abdefee34994f05ebda5dd13634e0a5700e1ad
sajicode <sajioloye@gmail.com>
2019-06-10 23:56:36 +0100

    setup methods on user model

--- dist/models/User.js
+++ dist/models/User.js
@@ -1,6 +1,10 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
+const bcrypt_1 = __importDefault(require("bcrypt"));
 const UserSchema = new mongoose_1.Schema({
     firstName: {
         type: mongoose_1.SchemaTypes.String
@@ -8,8 +12,36 @@ const UserSchema = new mongoose_1.Schema({
     lastName: { type: mongoose_1.SchemaTypes.String },
     email: {
         type: mongoose_1.SchemaTypes.String,
-        unique: true
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: mongoose_1.SchemaTypes.String
     },
     logNumber: { type: mongoose_1.SchemaTypes.Number }
 });
+// hash password before saving user
+UserSchema.pre('save', function (next) {
+    const user = this;
+    // generate a salt and run the callback
+    bcrypt_1.default.genSalt(10, function (err, salt) {
+        if (err)
+            return next(err);
+        // encrypt the password using the salt
+        bcrypt_1.default.hash(user.password, salt, function (err, hash) {
+            if (err)
+                return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+UserSchema.methods.comparePassword = function (userPassword, callback) {
+    bcrypt_1.default.compare(userPassword, this.password, function (err, isMatch) {
+        if (err)
+            return callback(err);
+        callback(null, isMatch);
+    });
+};
+// create and export the model class
 exports.default = mongoose_1.model('Users', UserSchema);
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,15 +1,19 @@
 import { Schema, SchemaTypes, Document, model } from 'mongoose';
+import { NextFunction } from 'express';
+import bcrypt from 'bcrypt';
 
 interface User {
 	id: string;
 	firstName: string;
 	lastName: string;
 	email: string;
+	password: string;
 	logNumber: number;
 }
 
 export interface UserDocument extends User, Document {
 	id: string;
+	password: string;
 }
 
 const UserSchema = new Schema({
@@ -19,9 +23,40 @@ const UserSchema = new Schema({
 	lastName: { type: SchemaTypes.String },
 	email: {
 		type: SchemaTypes.String,
-		unique: true
+		unique: true,
+		lowercase: true
+	},
+	password: {
+		type: SchemaTypes.String
 	},
 	logNumber: { type: SchemaTypes.Number }
 });
 
+// hash password before saving user
+UserSchema.pre<UserDocument>('save', function(next: NextFunction) {
+	const user = this;
+
+	// generate a salt and run the callback
+	bcrypt.genSalt(10, function(err: any, salt: string) {
+		if (err) return next(err);
+
+		// encrypt the password using the salt
+		bcrypt.hash(user.password, salt, function(err: any, hash: string) {
+			if (err) return next(err);
+
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+UserSchema.methods.comparePassword = function(userPassword: string, callback: any) {
+	bcrypt.compare(userPassword, this.password, function(err, isMatch) {
+		if (err) return callback(err);
+
+		callback(null, isMatch);
+	});
+};
+
+// create and export the model class
 export default model<UserDocument>('Users', UserSchema);
