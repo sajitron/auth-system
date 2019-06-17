3291d5291b2c339be566d8409815728520311a5a
sajicode <sajioloye@gmail.com>
2019-06-18 00:07:44 +0100

    setup login action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
+// register user action
 export const register = (formProps, callback) => async (dispatch) => {
 	try {
 		const response = await axios.post('http//localhost:5000/register', formProps);
@@ -12,3 +13,18 @@ export const register = (formProps, callback) => async (dispatch) => {
 		dispatch({ type: AUTH_ERROR, payload: 'An Error Occured' });
 	}
 };
+
+// login user action
+export const login = (formProps, callback) => async (dispatch) => {
+	try {
+		const response = await axios.post('http://localhost:5000/signin', formProps);
+
+		dispatch({ type: AUTH_USER, payload: response.data.token });
+		localStorage.setItem('as-token', response.data.token);
+		callback();
+	} catch (error) {
+		dispatch({ type: AUTH_ERROR, payload: 'Invalid credentials' });
+	}
+};
+
+// todo setup logout action
