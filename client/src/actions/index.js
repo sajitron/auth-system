1ace9b1acac326b5adb77014d43c29bd446f6d4f
sajicode <sajioloye@gmail.com>
2019-06-20 23:50:27 +0100

    setup register component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,29 +1,30 @@
-import axios from 'axios';
+import ky from 'ky';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
 // register user action
 export const register = (formProps, callback) => async (dispatch) => {
-	try {
-		const response = await axios.post('http//localhost:5000/register', formProps);
+	const response = await ky.post('http//localhost:5000/register', { json: formProps }).json();
 
-		dispatch({ type: AUTH_USER, payload: response.data.token });
-		localStorage.setItem('as-token', response.data.token);
-		callback();
-	} catch (error) {
-		dispatch({ type: AUTH_ERROR, payload: 'An Error Occured' });
-	}
+	console.log(response);
+	// try {
+	// 	dispatch({ type: AUTH_USER, payload: response.data.token });
+	// 	localStorage.setItem('as-token', response.data.token);
+	// 	callback();
+	// } catch (error) {
+	// 	dispatch({ type: AUTH_ERROR, payload: response.data.error });
+	// }
 };
 
 // login user action
 export const login = (formProps, callback) => async (dispatch) => {
-	try {
-		const response = await axios.post('http://localhost:5000/signin', formProps);
+	const response = await ky.post('http://localhost:5000/signin', formProps);
 
+	try {
 		dispatch({ type: AUTH_USER, payload: response.data.token });
 		localStorage.setItem('as-token', response.data.token);
 		callback();
 	} catch (error) {
-		dispatch({ type: AUTH_ERROR, payload: 'Invalid credentials' });
+		dispatch({ type: AUTH_ERROR, payload: response.data.error });
 	}
 };
 
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+import React, { Component } from 'react';
+
+class Register extends Component {
+	onSubmit = (formProps) => {
+		this.props.register(formProps, () => {
+			this.props.history.push('/dahboard');
+		});
+	};
+
+	render() {
+		// destructure redux form handlesubmit from props
+		const { handleSubmit } = this.props;
+
+		return (
+			<form onSubmit={handleSubmit(this.onSubmit)}>
+				<fieldset>
+					<label>Email</label>
+					<Field name="email" type="text" component="input" autoComplete="none" />
+				</fieldset>
+				<fieldset>
+					<label>Password</label>
+					<Field name="password" type="password" component="input" autoComplete="none" />
+				</fieldset>
+				<div>{this.props.errorMessage}</div>
+				<button>Register</button>
+			</form>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		errorMessage: state.auth.errorMessage
+	};
+}
+
+export default compose(
+	connect(mapStateToProps, actions),
+	reduxForm({
+		form: 'register'
+	})
+)(Register);
